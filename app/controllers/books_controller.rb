101cbaa8ec566c35654b061aee266bcc0b9894cb
nadu1234 <146610305+nadu1234@users.noreply.github.com>
2023-10-18 10:49:04 +0000

    投稿、編集機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,51 @@
 class BooksController < ApplicationController
-  def creaat
+  def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      flash[:notice] = "Book was successfully created."
-      redirect_to books_path(@book.id)
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
     else
       render :index
     end
   end
 
   def index
+    @books = Book.all
     @book = Book.new
   end
 
   def show
+    @book_new = Book.new
+    @book = Book.find(params[:id])
+    @user = User.find(params[:id])
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    flash[:notice] = "Book was successfully destroyed."
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to books_path  
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
+  
+  
+  
   def edit
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books 
   end
+  
+  
+  
 end
