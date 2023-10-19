c3988abefd70b463620fce7ae1d588e0557ad558
nadu1234 <146610305+nadu1234@users.noreply.github.com>
2023-10-19 03:17:57 +0000

    リンク修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
   def show
     @book_new = Book.new
     @book = Book.find(params[:id])
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/about'
   root to: "homes#top"
   devise_for :users
-  resources :users, only: [:show, :edit, :index]
+  resources :users, only: [:show, :edit, :index, :update]
   resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
